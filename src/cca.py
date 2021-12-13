a109a38257d1c1243ec95558b9ec345a4f08c859
nieyuzhou <rucnyz@gmail.com>
2021-12-13 11:21:53 +0800

    featConcat

--- src/cca.py
+++ src/cca.py
@@ -6,7 +6,7 @@
 import argparse
 import os
 
-from cca_zoo.deepmodels import architectures, DCCAE, DCCA
+from mvlearn.embed import KMCCA
 from torch import nn
 from torch.utils.data import DataLoader
 
@@ -30,8 +30,8 @@ args = parser.parse_args()
 torch.manual_seed(123)  # 设置CPU生成随机数的种子，方便下次复现实验结果
 np.random.seed(123)
 
-train_dset = UCI_Dataset('train', args)
-eval_dset = UCI_Dataset('valid', args)
+train_dset = UKB_Dataset('train', args)
+eval_dset = UKB_Dataset('valid', args)
 # 设置好丢失模态
 missing_index = get_missing_index(args.views, args.num, args.missing_rate)
 train_dset.set_missing_index(missing_index[:int(args.num * 4 / 5)])
@@ -39,123 +39,128 @@ eval_dset.set_missing_index(missing_index[int(args.num * 4 / 5):])
 # 均值填充
 train_dset.replace_with_mean()
 eval_dset.replace_with_mean()
-# 设置loader
-train_loader = DataLoader(train_dset, batch_size = 400, shuffle = True,
-                          pin_memory = False)
-eval_loader = DataLoader(eval_dset, batch_size = 400, pin_memory = False)
-# 设置模型和优化器
-encoders = []
-decoders = []
-latent_dims = 100
-for ld in args.classifier_dims:
-    encoders.append(architectures.Encoder(latent_dims = latent_dims, feature_size = ld))
-    decoders.append(architectures.Decoder(latent_dims = latent_dims, feature_size = ld))
-dcca = DCCAE(latent_dims = latent_dims, encoders = encoders, decoders = decoders, r = 0.2)
-optim_cca = torch.optim.Adam(dcca.parameters(), lr = 0.01)
-bce_loss = nn.BCELoss()
-# 开始训练
-epochs = 100
-# 测试一下
-best_eval_accuracy = 0
-for epoch in range(epochs):
-    dcca.train(True)
-    loss_sum = 0
-    train_accuracy = 0
-    all_num = 0
-    # train
-    for step, (idx, X, y, missing_index) in enumerate(train_loader):
-        # 重建损失
-        loss = dcca.loss(list(X.values()))
-        # 分类损失
-        lsd_dim = 0
-        lsd = dcca(list(X.values()))
-        for i in range(args.views):
-            lsd_dim += lsd[i]
-        y_onehot = torch.zeros(y.shape[0], args.classes).scatter_(1, y.reshape(
-            y.shape[0], 1), 1)
-        loss2, _ = classification_loss(y_onehot, y, lsd_dim)
-        optim_cca.zero_grad()
-        loss.backward()
-        loss2.backward()
-        optim_cca.step()
-        loss_sum += loss2.item()
-        # 计算准确率
-        predicted = ave(lsd_dim, lsd_dim, y_onehot)
-        train_accuracy += accuracy_count(predicted, y)
-        all_num += y.size(0)
-    train_accuracy = train_accuracy / all_num
-    print("[Epoch %2d] loss: %.4f accuracy: %.4f" % (epoch + 1, loss_sum, (train_accuracy)))
-    # valid
-    valid_accuracy = 0
-    dcca.train(False)
-    with torch.no_grad():
-        for step, (idx, X, y, missing_index) in enumerate(eval_loader):
-            lsd_dim = 0
-            lsd = dcca(list(X.values()))
-            y_onehot = torch.zeros(y.shape[0], args.classes).scatter_(1, y.reshape(
-                y.shape[0], 1), 1)
-            for i in range(args.views):
-                lsd_dim += lsd[i]
-            predicted = ave(lsd_dim, lsd_dim, y_onehot)
-            valid_accuracy += accuracy_count(predicted, y)
-            all_num += y.size(0)
-        valid_accuracy = valid_accuracy / all_num
-        print("valid accuracy: %.4f" % valid_accuracy)
-        if valid_accuracy >= best_eval_accuracy:
-            best_eval_accuracy = valid_accuracy
-print("---------------------------------------------")
-print("Best evaluate accuracy:{}".format(best_eval_accuracy))
-# 预处理数据集
-# X = list(train_dset.full_data.values())
-# X_valid = list(eval_dset.full_data.values())
-# y = train_dset.full_labels
-# y_valid = eval_dset.full_labels
-# # 使用CCA方法得到隐藏层
-# components = 4
-# model = DCCA(components)
-# mcca = KMCCA(n_components = components, regs = 0.1)
-# scores_train = mcca.fit_transform(X)
-# s_train = np.empty((scores_train.shape[1], scores_train.shape[0] * scores_train.shape[2]))
-# for i in range(scores_train.shape[0]):
-#     s_train[:, i * components:(i + 1) * components] = scores_train[i]
-# # 验证集隐藏层
-# scores_valid = mcca.fit_transform(X_valid)
-# s_valid = np.empty((scores_valid.shape[1], scores_valid.shape[0] * scores_valid.shape[2]))
-# for i in range(scores_train.shape[0]):
-#     s_valid[:, i * components:(i + 1) * components] = scores_valid[i]
-# # 设置为tensor
-# s_train = torch.from_numpy(s_train).float()
-# s_valid = torch.from_numpy(s_valid).float()
-#
-# # 跑一个简单的模型
-# net = nn.Sequential(
-#     nn.Linear(scores_train.shape[0] * scores_train.shape[2], 128),
-#     nn.ReLU(),
-#     nn.Linear(128, y.max() - y.min() + 1),
-#     nn.Softmax(dim = 1)
-# )
-# loss_fn = nn.CrossEntropyLoss()
-# optim = torch.optim.Adam(net.parameters(), lr = 0.1)
-# # 开始运行
-# best_valid_acc = 0
-# for i in range(200):
-#     predicted = net(s_train)
-#     loss = loss_fn(predicted, y)
-#     optim.zero_grad()
-#     loss.backward()
-#     optim.step()
-#     # 计算准确率
-#     y_predicted = torch.argmax(predicted, dim = 1)
-#     train_acc = ((y_predicted == y).sum() / y.shape[0] * 100).item()
-#     # 验证部分
+
+# ----------------------------------------
+# # 设置loader
+# train_loader = DataLoader(train_dset, batch_size = 400, shuffle = True,
+#                           pin_memory = False)
+# eval_loader = DataLoader(eval_dset, batch_size = 400, pin_memory = False)
+# # 设置模型和优化器
+# encoders = []
+# decoders = []
+# latent_dims = 100
+# for ld in args.classifier_dims:
+#     encoders.append(architectures.Encoder(latent_dims = latent_dims, feature_size = ld))
+#     decoders.append(architectures.Decoder(latent_dims = latent_dims, feature_size = ld))
+# dcca = DCCAE(latent_dims = latent_dims, encoders = encoders, decoders = decoders, r = 0.2)
+# optim_cca = torch.optim.Adam(dcca.parameters(), lr = 0.01)
+# bce_loss = nn.BCELoss()
+# # 开始训练
+# epochs = 100
+# # 测试一下
+# best_eval_accuracy = 0
+# for epoch in range(epochs):
+#     dcca.train(True)
+#     loss_sum = 0
+#     train_accuracy = 0
+#     all_num = 0
+#     # train
+#     for step, (idx, X, y, missing_index) in enumerate(train_loader):
+#         # 重建损失
+#         loss = dcca.loss(list(X.values()))
+#         # 分类损失
+#         lsd_dim = 0
+#         lsd = dcca(list(X.values()))
+#         for i in range(args.views):
+#             lsd_dim += lsd[i]
+#         y_onehot = torch.zeros(y.shape[0], args.classes).scatter_(1, y.reshape(
+#             y.shape[0], 1), 1)
+#         loss2, _ = classification_loss(y_onehot, y, lsd_dim)
+#         optim_cca.zero_grad()
+#         loss.backward()
+#         loss2.backward()
+#         optim_cca.step()
+#         loss_sum += loss2.item()
+#         # 计算准确率
+#         predicted = ave(lsd_dim, lsd_dim, y_onehot)
+#         train_accuracy += accuracy_count(predicted, y)
+#         all_num += y.size(0)
+#     train_accuracy = train_accuracy / all_num
+#     print("[Epoch %2d] loss: %.4f accuracy: %.4f" % (epoch + 1, loss_sum, (train_accuracy)))
+#     # valid
+#     valid_accuracy = 0
+#     dcca.train(False)
 #     with torch.no_grad():
-#         predicted_valid = net(s_valid)
-#         y_predicted_valid = torch.argmax(predicted_valid, dim = 1)
-#         valid_acc = ((y_predicted_valid == y_valid).sum() / y_valid.shape[0] * 100).item()
-#     if best_valid_acc < valid_acc:
-#         best_valid_acc = valid_acc
-#     print("第{}个epoch：\n训练准确率:{:.4f}     验证准确率:{:.4f}".format(i, train_acc, valid_acc))
-# print("------------------------------\n最高验证集准确率:{:.4f}".format(best_valid_acc))
+#         for step, (idx, X, y, missing_index) in enumerate(eval_loader):
+#             lsd_dim = 0
+#             lsd = dcca(list(X.values()))
+#             y_onehot = torch.zeros(y.shape[0], args.classes).scatter_(1, y.reshape(
+#                 y.shape[0], 1), 1)
+#             for i in range(args.views):
+#                 lsd_dim += lsd[i]
+#             predicted = ave(lsd_dim, lsd_dim, y_onehot)
+#             valid_accuracy += accuracy_count(predicted, y)
+#             all_num += y.size(0)
+#         valid_accuracy = valid_accuracy / all_num
+#         print("valid accuracy: %.4f" % valid_accuracy)
+#         if valid_accuracy >= best_eval_accuracy:
+#             best_eval_accuracy = valid_accuracy
+# print("---------------------------------------------")
+# print("Best evaluate accuracy:{}".format(best_eval_accuracy))
+
+# ----------------------------------------
+
+# 预处理数据集
+X = list(train_dset.full_data.values())
+X_valid = list(eval_dset.full_data.values())
+y = train_dset.full_labels
+y_valid = eval_dset.full_labels
+# 使用CCA方法得到隐藏层
+components = 10
+mcca = KMCCA(n_components = components, regs = 0.1)
+scores_train = mcca.fit_transform(X)
+s_train = np.empty((scores_train.shape[1], scores_train.shape[0] * scores_train.shape[2]))
+for i in range(scores_train.shape[0]):
+    s_train[:, i * components:(i + 1) * components] = scores_train[i]
+# 验证集隐藏层
+scores_valid = mcca.fit_transform(X_valid)
+s_valid = np.empty((scores_valid.shape[1], scores_valid.shape[0] * scores_valid.shape[2]))
+for i in range(scores_train.shape[0]):
+    s_valid[:, i * components:(i + 1) * components] = scores_valid[i]
+# 设置为tensor
+s_train = torch.from_numpy(s_train).float()
+s_valid = torch.from_numpy(s_valid).float()
+
+# 跑一个简单的模型
+net = nn.Sequential(
+    nn.Linear(scores_train.shape[0] * scores_train.shape[2], 128),
+    nn.ReLU(),
+    nn.Linear(128, y.max() - y.min() + 1),
+    nn.Softmax(dim = 1)
+)
+loss_fn = nn.CrossEntropyLoss()
+optim = torch.optim.Adam(net.parameters(), lr = 0.1)
+# 开始运行
+best_valid_acc = 0
+for i in range(200):
+    predicted = net(s_train)
+    loss = loss_fn(predicted, y)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+    # 计算准确率
+    y_predicted = torch.argmax(predicted, dim = 1)
+    train_acc = ((y_predicted == y).sum() / y.shape[0] * 100).item()
+    # 验证部分
+    with torch.no_grad():
+        predicted_valid = net(s_valid)
+        y_predicted_valid = torch.argmax(predicted_valid, dim = 1)
+        valid_acc = ((y_predicted_valid == y_valid).sum() / y_valid.shape[0] * 100).item()
+    if best_valid_acc < valid_acc:
+        best_valid_acc = valid_acc
+    print("第{}个epoch：\n训练准确率:{:.4f}     验证准确率:{:.4f}".format(i, train_acc, valid_acc))
+print("------------------------------\n最高验证集准确率:{:.4f}".format(best_valid_acc))
+# ----------------------------------------
 # 使用SVM测试
 # clf = SVC().fit(s, y)
 # y_predict = clf.predict(s)
@@ -163,7 +168,7 @@ print("Best evaluate accuracy:{}".format(best_eval_accuracy))
 # print("训练集:\n svm:{:.4f}".format(svm_acc))
 # -------------------------------------
 # 验证集试试
-
+#
 # y_predict = clf.predict(s_valid)
 # svm_acc = accuracy_score(y_valid, y_predict)
 # print("验证集:\n svm:{:.4f}".format(svm_acc))
--- src/other.py
+++ src/other.py
@@ -4,30 +4,38 @@
 # @File    : other.py
 # @Software: PyCharm
 import argparse
+import os
 
+from torch import nn
 from torch.utils.data import DataLoader
 
+from predict_model.net import FeatConcat
+from utils.make_optim import Adam
 from utils.pred_func import *
 from utils.preprocess import get_missing_index, missing_data_process
+from dataset.UKB_dataset import UKB_Dataset
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output', type = str, default = 'ckpt/')
     parser.add_argument('--name', type = str, default = 'exp0/')
-    parser.add_argument('--sets', nargs = '+', default = ["valid", "test"])
     parser.add_argument('--num_workers', type = int, default = 0)
-    parser.add_argument('--index', type = int, default = 99)
-
+    parser.add_argument('--dataset', type = str,
+                        choices = ['Caltech101_7', 'Caltech101_20', 'Reuters', 'NUSWIDEOBJ', 'MIMIC', 'UCI', 'UKB'],
+                        default = 'UKB')
+    parser.add_argument('--missing_rate', type = float, default = 0,
+                        help = 'view missing rate [default: 0]')
+    parser.add_argument('--seed', type = int, default = 123)
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
-    model_path = "ckpt/mymodel/best123UKB.pkl"
-    # Load original args
-    state = torch.load(model_path)
-    args = state['args']
+    if os.getcwd().endswith("src"):
+        os.chdir("../")
+    args = parse_args()
+    args.dataloader = "UKB_Dataset"
     # 设置seed
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
@@ -38,23 +46,65 @@ if __name__ == '__main__':
     train_dset = eval(args.dataloader)('train', args)  # args.dataloader类的构造函数
     eval_dset = eval(args.dataloader)('valid', args)
 
-    # Generate missing views
+    # 设置好丢失模态
     missing_index = get_missing_index(args.views, args.num, args.missing_rate)
-
-    # Preprocess data with missing views and load data
-    missing_data_process(args, train_dset, eval_dset, missing_index)
+    train_dset.set_missing_index(missing_index[:int(args.num * 4 / 5)])
+    eval_dset.set_missing_index(missing_index[int(args.num * 4 / 5):])
+    # 均值填充
+    train_dset.replace_with_mean()
+    eval_dset.replace_with_mean()
 
     # DataLoader: DataSet的打包
-    train_loader = DataLoader(train_dset, args.train_batch_size, num_workers = args.num_workers, shuffle = True,
+    train_loader = DataLoader(train_dset, 400, num_workers = args.num_workers, shuffle = True,
                               pin_memory = False)
-    eval_loader = DataLoader(eval_dset, args.valid_batch_size, num_workers = args.num_workers, pin_memory = False)
+    eval_loader = DataLoader(eval_dset, 400, num_workers = args.num_workers, pin_memory = False)
     # batch_size:一次运行多少个sample
     # shuffle是打乱顺序: True两次顺序不同，False两次顺序相同，默认为false
     # num_workers: 采用多进程进行加载，默认为0，即逐进程；>0在windows下会出现错误
     # drop_last: 总sample除以batch_size除不尽的时候True舍弃
     # pin_memory: If True, the data loader will copy tensors into CUDA pinned memory before returning them.
     # ctrl+p可以查看参数
-
+    epochs = 100
     # Net
-    net = eval(args.model)(args)  # choices = ["CPM", "CPM_GAN", "TMC"]
-    net.to(args.device)  # 是否用GPU，将模型加载到相应的设备中
+    net = FeatConcat(args)  # choices = ["CPM", "CPM_GAN", "TMC"]
+    # 优化器
+    optim = Adam(net, 0.01)
+    # 损失函数
+    loss_fn = nn.BCELoss()
+    best_eval_accuracy = 0
+    for epoch in range(epochs):
+        net.train(True)
+        loss_sum = 0
+        train_accuracy = 0
+        all_num = 0
+        # train
+        for step, (idx, X, y, missing_index) in enumerate(train_loader):
+            # 重建损失
+            output = net(X)
+            predicted = torch.argmax(output, dim = 1)
+            y_onehot = torch.zeros(y.shape[0], args.classes).scatter_(1, y.reshape(
+                y.shape[0], 1), 1)
+            loss = loss_fn(output, y_onehot)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            # 计算准确率
+            train_accuracy += accuracy_count(predicted, y)
+            all_num += y.size(0)
+        train_accuracy = train_accuracy / all_num
+        print("[Epoch %2d] loss: %.4f accuracy: %.4f" % (epoch + 1, loss_sum, (train_accuracy)))
+        # valid
+        valid_accuracy = 0
+        net.train(False)
+        with torch.no_grad():
+            for step, (idx, X, y, missing_index) in enumerate(eval_loader):
+                output = net(X)
+                predicted = torch.argmax(output, dim = 1)
+                valid_accuracy += accuracy_count(predicted, y)
+                all_num += y.size(0)
+            valid_accuracy = valid_accuracy / all_num
+            print("valid accuracy: %.4f" % valid_accuracy)
+            if valid_accuracy >= best_eval_accuracy:
+                best_eval_accuracy = valid_accuracy
+    print("---------------------------------------------")
+    print("Best evaluate accuracy:{}".format(best_eval_accuracy))
